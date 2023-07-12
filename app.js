d7d79abfdae27f7ccc229fb98ea5637738f6629d
Armaan Sharma <armaansharma1701@gmail.com>
2023-07-12 09:56:58 -0400

    make final changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app=express();
 const _=require("lodash");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"))
-mongoose.connect("mongodb+srv://armaansharma1701:T3M1N4TOR@cluster0.a5szzkn.mongodb.net/todolistDB",{useNewUrlParser:true})
+mongoose.connect("mongodb+srv://armaansharma1701:T3M1N4TOR@cluster0.a5szzkn.mongodb.net/todolistDB",{useNewUrlParser:true})   //connection to mongoDB ATLAS
 
 app.set('view engine', 'ejs');   //initializing ejs
 const itemsSchema=new mongoose.Schema({
@@ -38,10 +38,7 @@ const listSchema={
 const List=mongoose.model("List",listSchema);
 
 app.get("/",function(req,res){
-    
-     //let today=date();
-     /*items are found and checked if the length of the items list is 0. If items list is empty, then default items are added using
-     insertMany method and the you get redirected to the home route. If items list is not empty then the res.render takes place*/
+
      Item.find({}).exec()
      .then((items) => {
         if (items.length===0){
@@ -59,16 +56,13 @@ app.get("/",function(req,res){
 })
 
 app.get("/:customListName",function(req,res){
-    const customListName=_.capitalize(req.params.customListName);              //this allows us to get the name of any route the user enters after localhost:3000/""
-    //lodash .capitalize is used to capitalize the first letter so that it the route becomes case insensitive
-    /*to find if the name of the route(eg: /home) exists in the database. If it does then we just render the ejs file using the name
-    of the 'found' route as the title and the default items in the list as found.items*/ 
+    const customListName=_.capitalize(req.params.customListName);              
     List.findOne({name:customListName})
     .then((found) => {
         if (!found){
             const list=new List({
                 name:customListName,
-                items:defaultItems   //array of items is the same as default items for our new route
+                items:defaultItems   
             })
             list.save();
             res.redirect("/"+customListName);  //redirects to the customListName route after saving the data
@@ -86,7 +80,7 @@ app.get("/:customListName",function(req,res){
 
 app.post("/",function(req,res){
     //adding new items
-    const listName=req.body.list;   //this line is required to know which route the plus button has been clicked
+    const listName=req.body.list;   
     const itemName=req.body.NewItem;
     const item=new Item({
         name:itemName
@@ -107,8 +101,7 @@ app.post("/",function(req,res){
         })
 
     }
-        /* at this step, after the post request is made, the server redirects to the home route
-                                where it encounters a get request and  renders the ejs file in the app.get method*/
+        
     
 })
 
@@ -120,7 +113,7 @@ app.post("/delete",function(req,res){
         console.log("successfully deleted");
         res.redirect("/"); 
     }else{                           
-        List.findOneAndUpdate({name:listName},{$pull:{items:{_id:checkedItemId}}},{new:true})   //$pull is a mongoDB method to remove data from a field (items)that match the query(id)
+        List.findOneAndUpdate({name:listName},{$pull:{items:{_id:checkedItemId}}},{new:true})   
         .then((found)=>{
             res.redirect("/"+listName)
         })
@@ -132,7 +125,7 @@ app.post("/delete",function(req,res){
   .then((deletedItem) => {
     if (deletedItem) {
       console.log("successfully deleted");
-      res.redirect("/");       //after deleting from the database, you redirect to the home route for it to check through the db items again and display them
+      res.redirect("/");       
     }
   })
   .catch((error) => {
@@ -142,11 +135,7 @@ app.post("/delete",function(req,res){
 
 })
 
-let port=process.env.PORT;
-if (port==null|| port==""){
-    port=3000;
-}
 
-app.listen(port,function(){
+app.listen("3000",function(){       //runs on local computer
     console.log("server is up and running");
 })
